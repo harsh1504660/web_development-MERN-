7343df35fe14d872f2d12c87e81cb289c2ceaef2
Harsh Joshi <harshsmj1504@gmail.com>
2024-07-19 00:00:07 +0530

    add day 33

---
+++ day_33_(conditions_js)/script.js
@@ -0,0 +1,16 @@
+console.log("hello i am condtional tutorial")
+
+let age =45;
+let grace = 2;
+if (age > 18){
+    console.log("you can drive");
+}
+else{
+    console.log("you can not drive");
+}
+
+
+let a="3";
+let b = 3;
+console.log(a==b); /// output is true
+console.log(a===b); /// output is false
