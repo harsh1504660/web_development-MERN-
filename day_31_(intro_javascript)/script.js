9d208a3c525de586aa5d4088ec800e595f424efe
Harsh Joshi <harshsmj1504@gmail.com>
2024-07-18 21:10:50 +0530

    add day 31

---
+++ day_31_(intro_javascript)/script.js
@@ -0,0 +1,16 @@
+
+alert('hello world');
+
+console.log("code is running")
+
+
+var a = prompt("enter a number")
+var isTrue = confirm('sure exit?')
+
+if (isTrue){
+    console.log("existing")
+}
+else{
+    console.log("not existing")
+}
+console.log("your number is" +a)
