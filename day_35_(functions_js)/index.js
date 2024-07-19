55fe74f80b8a49dcaad4d01394d129c76b098904
Harsh Joshi <harshsmj1504@gmail.com>
2024-07-19 21:35:19 +0530

    add day 35 and 36

---
+++ day_35_(functions_js)/index.js
@@ -0,0 +1,17 @@
+
+
+function nice(name){
+    console.log("hey"+name+"your nice ")
+    console.log(name +"your shirt is nice")
+    console.log("hey" +name+"your shirt is nice and nice")
+    console.log("hey" +name +"your shirt is nice and nice and nice")
+}
+
+nice("rohan")
+
+
+const func1 = (x) => {
+    console.log("i am a arrow function",x)
+}
+
+func1(34);
---
+++ day_36_(strings_js)/index.js
@@ -0,0 +1,20 @@
+console.log("this is tutorial")
+let a ="harsh";
+console.log(a[0])
+
+
+console.log(a.length)
+
+let name_ = "harsh";
+let frend = "rohan";
+console.log("this name is "+name_+"and frend is"+frend)
+
+console.log(`this is ${name_} and frend is ${frend}`) // string interpolation
+
+console.log(name_.toLocaleUpperCase())
+
+// string slicing 
+console.log(name_.slice(0,2))
+
+console.log(name_.replace("sh","77"))
+console.log(name_.concat(frend,a))
