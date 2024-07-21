8dd47ddd5f6fcbaf261b60c48471fb3c611567ea
Harsh Joshi <harshsmj1504@gmail.com>
2024-07-21 23:53:20 +0530

    add day 49

---
+++ day_49_(ECMAS_Common_js)/main.js
@@ -0,0 +1,13 @@
+// const { createServer } = require('node:http');
+// const fs = require("fs")    // importing
+
+
+import {a,b} from "./mymodule.js"
+console.log(a,b)
+
+
+import obj from "./mymodule.js"
+console.log(obj)
+
+const z = require("./mymodule2.js")
+console.log(z)
---
+++ day_49_(ECMAS_Common_js)/mymodule.js
@@ -0,0 +1,8 @@
+export const a = 1  // name export
+export const b = 2
+
+let obj;
+export default obj = {
+    x:5,
+    y:7
+}
