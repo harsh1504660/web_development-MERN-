87f4a45cd4512e6af63aa68d6c01b26342238510
Harsh Joshi <harshsmj1504@gmail.com>
2024-08-04 01:23:51 +0530

    add day 72

---
+++ day_72_(intro_to_next.js)/first/app/about/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+const page = () => {
+  return (
+    <div>
+     <h1 className="text-xl">
+        Problem solved by Next.js
+     </h1>
+     <ul>
+        <li>Full stack solution</li>
+        <li>File Based routing</li>
+        <li>additional featuers from navigation</li>
+        <li>optimized rendering</li>
+     </ul>
+    
+    </div>
+  )
+}
+
+export default page
