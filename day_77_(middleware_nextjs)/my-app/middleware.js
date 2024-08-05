b7abad3c166583ff053cf6338f3418d7fbda2c0e
Harsh Joshi <harshsmj1504@gmail.com>
2024-08-05 17:58:25 +0530

    ad day 77

---
+++ day_77_(middleware_nextjs)/my-app/middleware.js
@@ -0,0 +1,9 @@
+import { Router } from "next/router";
+import { NextResponse } from "next/server";
+
+export function middleware(request){
+    return NextResponse.redirect(new URL('/',request.url))
+}
+export const config ={
+    matcher :'/about/:path'
+}
